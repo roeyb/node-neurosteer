027600987c3e0c1775f1e4daf62cd50c05a1bbbb
roeybr <roey.brecher@gmail.com>
2015-07-07 00:47:07 +0300

    made it work

--- index.js
+++ index.js
@@ -1,28 +1,23 @@
-var socket;
-var WebSocket = require('websocket-client').WebSocket;
+var WebSocket = require('ws');
+var ws = new WebSocket('ws://cloud.neurosteer.com:8080/v1/features/0006664e5c15/pull', {
+  protocolVersion: 8,
+  origin: 'http://websocket.org'
+});
 
-var reconnectInterval = 1000 * 60;
-var connect = function () {
-    var count = 0;
+ws.on('open', function open() {
+  console.log('connected');
+});
 
-    socket = new WebSocket( "ws://cloud.neurosteer.com:8080/v1/features/0006664e5c15/pull" );
-    socket.onmessage = function ( event ) {
-        record = eval( "(" + event.data + ")" ); // convert to JSON
-        console.log(  record.features );
-    };
-    socket.onopen = function ( event ) {
-        var ta = document.getElementById( 'responseText' );
-        console.log(   "Web Socket opened!" );
-    };
-    socket.onerror = function () {
-        console.log( 'socket error' );
-        setTimeout( connect, reconnectInterval );
-    };
-    socket.onclose = function ( event ) {
-        console.log( 'socket close' );
-        var ta = document.getElementById( 'responseText' );
-        console.log(  "Web Socket closed, reconnect in " + reconnectInterval + " msec" );
-        setTimeout( connect, reconnectInterval );
-    };
-};
-connect();
+ws.on('close', function close() {
+  console.log('disconnected');
+});
+
+ws.on('error', function close() {
+  console.log('socket error');
+  //todo reconnect
+});
+
+ws.on('message', function message(data, flags) {
+  var record = eval( "(" + data + ")" ); // convert to JSON
+  console.log(  record.features.c1 );
+});
